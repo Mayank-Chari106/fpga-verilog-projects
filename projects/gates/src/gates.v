64097e2abd4db22c13dd6505dbe5a6c79490f49e
Mayank Chari <mayank@email.com>
2026-05-16 16:47:18 +0530

    modified:   projects/gates/src/gates.v

--- projects/gates/src/gates.v
+++ projects/gates/src/gates.v
@@ -1,49 +1,52 @@
-(* top *) module Gates(
- (* iopad_external_pin, clkbuf_inhibit *) input clk,
+ module Gates(
 
- (* iopad_external_pin *) output reg Y = 0,
- (* iopad_external_pin *) output Y_en ,
+    (* iopad_external_pin, clkbuf_inhibit *) input clk,
 
- (* iopad_external_pin *) input A ,
- (* iopad_external_pin *) output A_en ,
+    (* iopad_external_pin *) output reg Y = 0,
+    (* iopad_external_pin *) output Y_en ,
 
- (* iopad_external_pin *) input B ,
- (* iopad_external_pin *) output B_en ,
+    (* iopad_external_pin *) input A ,
+    (* iopad_external_pin *) output A_en ,
 
- (* iopad_external_pin *) input S0 ,
- (* iopad_external_pin *) output S0_en ,
+    (* iopad_external_pin *) input B ,
+    (* iopad_external_pin *) output B_en ,
 
- (* iopad_external_pin *) input S1 ,
- (* iopad_external_pin *) output S1_en ,
+    (* iopad_external_pin *) input S0 ,
+    (* iopad_external_pin *) output S0_en ,
 
- (* iopad_external_pin *) input S2 ,
- (* iopad_external_pin *) output S2_en 
- );
+    (* iopad_external_pin *) input S1 ,
+    (* iopad_external_pin *) output S1_en ,
 
- assign Y_en  = 1'b1;
- assign A_en  = 1'b0;
- assign B_en  = 1'b0;
- assign S0_en = 1'b0;
- assign S1_en = 1'b0;
- assign S2_en = 1'b0;
+    (* iopad_external_pin *) input S2 ,
+    (* iopad_external_pin *) output S2_en
+);
 
- always @(*) begin
+assign Y_en  = 1'b1;
 
-     case ({S2,S1,S0})
+assign A_en  = 1'b0;
+assign B_en  = 1'b0;
 
-         3'd0: Y = A&B;      
-         3'd1: Y = A | B;      
-         3'd2: Y = ~(A & B);   
-         3'd3: Y = ~(A | B);   
-         3'd4: Y = A ^ B;      
-         3'd5: Y = ~(A ^ B);   
-         3'd6: Y = ~A;         
-         3'd7: Y = ~B;         
+assign S0_en = 1'b0;
+assign S1_en = 1'b0;
+assign S2_en = 1'b0;
 
-         default: Y = 1'b0;
+always @(*) begin
 
-     endcase
+    case ({S2,S1,S0})
 
- end
+        3'b000: Y = A & B;
+        3'b001: Y = A | B;
+        3'b010: Y = A ^ B;
+        3'b011: Y = ~(A & B);
+        3'b100: Y = ~(A | B);
+        3'b101: Y = A ~^ B;
+        3'b110: Y = ~A;
+        3'b111: Y = ~B;
 
- endmodule
+        default: Y = 1'b0;
+
+    endcase
+
+end
+
+endmodule
